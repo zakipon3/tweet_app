93817b79a5778d9be12f7f97e84867fb64aae73a
zakipon3 <miyapon3@gmail.com>
2020-07-08 20:59:26 +0900

    markup hpheader

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
+
+  resources :users, only: [:index, :new, :edit, :show, :update] do
+
+  end
 end
